d3a29f62c891fddc47a8e18cdefdaff5457eaff1
Gino804 <giacomo.p03@gmail.com>
2023-06-28 15:39:59 +0200

    Created AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="jumbotron">
+        <img src="../assets/crusaders.jpg" alt="Stardust Crusaders">
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    width: 70vw;
+    margin: auto;
+}
+
+.jumbotron>img {
+    width: 100%;
+}
+</style>
